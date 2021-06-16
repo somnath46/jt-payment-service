4b60f64b99d781b35360585adc8b6a49f6f6c36f
somnath46 <4684.somnath@gmail.com>
2021-06-16 19:55:17 +0530

    initial project setup #1

---
+++ src/main/java/com/jt/payment/JtPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jt.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jt/payment/JtPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
