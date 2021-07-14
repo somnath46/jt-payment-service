821418c02026af8cd96294c3cba20b667b7e5e47
somnath46 <4684.somnath@gmail.com>
2021-07-14 17:57:21 +0530

    add logs and all logs should go to a log file #7

--- src/main/java/com/jt/payment/service/PaymentService.java
+++ src/main/java/com/jt/payment/service/PaymentService.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.jt.payment.dto.PaymentDto;
@@ -20,12 +20,15 @@ public class PaymentService {
 	@Autowired
 	private PaymentRepository paymentRepository;
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(PaymentService.class);
+
 	public PaymentDto doPayment(PaymentDto paymentDto) {
 		Payment payment = new Payment(paymentDto.getId(), paymentDto.getPaymentStatus(), paymentDto.getTransactionId(),
 				paymentDto.getOrderId(), paymentDto.getAmount());
 		payment.setPaymentStatus(processPaymentStatus());
 		payment.setTransactionId(UUID.randomUUID().toString());
 		Payment savedPayment = paymentRepository.save(payment);
+		LOGGER.info("Payment processed with id: {}", savedPayment.getId());
 
 		return new PaymentDto(savedPayment.getId(), savedPayment.getPaymentStatus(), savedPayment.getTransactionId(),
 				savedPayment.getOrderId(), savedPayment.getAmount());
@@ -42,8 +45,8 @@ public class PaymentService {
 			return null;
 		}
 		List<PaymentDto> dtos = new ArrayList<>();
-		payments.forEach(payment -> dtos.add(new PaymentDto(payment.getId(), payment.getPaymentStatus(), payment.getTransactionId(),
-				payment.getOrderId(), payment.getAmount())));
+		payments.forEach(payment -> dtos.add(new PaymentDto(payment.getId(), payment.getPaymentStatus(),
+				payment.getTransactionId(), payment.getOrderId(), payment.getAmount())));
 
 		return dtos;
 	}
